e61736b3290e60e8b04fff47d747614ed673b5f8
Deathslayer89 <dineshswain2001@gmail.com>
2025-02-28 08:23:34 +0530

    app.config changes

---
+++ app.config.js
@@ -0,0 +1,57 @@
+// app.config.js
+module.exports = () => {
+    require('dotenv').config();
+  
+    return {
+      expo: {
+        name: "ImageAnalyzer",
+        slug: "image-analyzer",
+        version: "1.0.0",
+        orientation: "portrait",
+        icon: "./assets/images/icon.png",
+        scheme: "myapp",
+        userInterfaceStyle: "automatic",
+        newArchEnabled: true,
+        ios: {
+          supportsTablet: true,
+        },
+        web: {
+          bundler: "metro",
+          output: "single",
+          favicon: "./assets/images/favicon.png",
+        },
+        plugins: [
+          "expo-router",
+          "expo-secure-store",
+          [
+            "expo-camera",
+            {
+              cameraPermission: "Allow $(PRODUCT_NAME) to access your camera",
+              microphonePermission: "Allow $(PRODUCT_NAME) to access your microphone",
+              recordAudioAndroid: true,
+            },
+          ],
+        ],
+        experiments: {
+          typedRoutes: true,
+        },
+        extra: {
+          router: {
+            origin: false,
+          },
+          eas: {
+            projectId: "a687ad65-a539-4097-a882-e6b73f0e5b5c",
+          },
+          GeminiApiKey: process.env.EXPO_PUBLIC_GEMINI_API_KEY || "default-dev-key",
+          SupabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "development",
+          SupabaseURL: process.env.EXPO_PUBLIC_SUPABASE_URL || "development",
+        },
+        android: {
+          package: "com.deathslayer89.imageanalyzer",
+          adaptiveIcon: {
+            foregroundImage: "./assets/images/icon.png" // Use icon.png for adaptive icon
+          }
+        },
+      },
+    };
+  };
